58a2333c3280a986e16cd93677f450a83bfb5f43
Iaroslava Goncharova <111697910+iaroslavagoncharova@users.noreply.github.com>
2024-03-12 21:31:31 +0200

    Update database.sql

--- database.sql
+++ database.sql
@@ -51,6 +51,8 @@ CREATE TABLE Tests (
     test_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     description TEXT,
+    isDefault INT,
+    link VARCHAR(255),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 );
 
