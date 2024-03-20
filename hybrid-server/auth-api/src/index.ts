44acd5d337d809ff3af3f406474354a9d1741cb7
Iaroslava Goncharova <111697910+iaroslavagoncharova@users.noreply.github.com>
2024-03-20 16:15:46 +0200

    servers removed

--- hybrid-server/auth-api/src/index.ts
+++
@@ -1,7 +0,0 @@
-import app from './app';
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  /* eslint-disable no-console */
-  console.log(`Listening: http://localhost:${port}`);
-  /* eslint-enable no-console */
-});
