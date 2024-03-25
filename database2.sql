2c8beef2ece0d170773baf4e09e7c44109a8b451
Iaroslava Goncharova <111697910+iaroslavagoncharova@users.noreply.github.com>
2024-03-25 20:55:35 +0200

    mock data added

--- database2.sql
+++ database2.sql
@@ -32,7 +32,7 @@ CREATE TABLE JobExperience (
   job_city varchar(255),
   description varchar(255),
   start_date date NOT NULL,
-  end_date date NOT NULL,
+  end_date date,
   FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );
 
@@ -48,7 +48,7 @@ CREATE TABLE Education (
 
 CREATE TABLE Attachments (
   attachment_id int(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  atachment_name int(11) NOT NULL,
+  attachment_name varchar(255) NOT NULL,
   user_id int(11) NOT NULL,
   link varchar(255) NOT NULL,
   FOREIGN KEY (user_id) REFERENCES Users (user_id)
@@ -105,7 +105,7 @@ CREATE TABLE Applications (
   job_id int(11) NOT NULL,
   status varchar(255) NOT NULL,
   application_text text,
-  created_at date NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   FOREIGN KEY (user_id) REFERENCES Users (user_id),
   FOREIGN KEY (job_id) REFERENCES JobAds (job_id)
 );
@@ -200,3 +200,31 @@ CREATE TABLE Notifications (
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   FOREIGN KEY (match_id) REFERENCES Matches(match_id)
 );
+
+
+INSERT INTO UserLevels (level_name) VALUES ("Candidate"), ("Employer"), ("Admin");
+INSERT INTO Users (username, password, email, user_level_id, fullname, phone, user_type) VALUES ("Slava", "$2a$10$0.XQQChT9PUlSLKhXe4kweNqkCqcdizWVh45CmZxJ5gEPx5EWm/J2", "slava@example.com", 1, "Iaroslava Goncharova", "+358410000000", "candidate");
+INSERT INTO Users (username, password, email, user_level_id, fullname, phone, user_type) VALUES ("Kamilla", "$2a$10$0.XQQChT9PUlSLKhXe4kweNqkCqcdizWVh45CmZxJ5gEPx5EWm/J2", "kamilla@example.com", 2, "Kamilla Karenius", "+358410000000", "employer");
+INSERT INTO Users (username, password, email, user_level_id, fullname, phone, user_type) VALUES ("Anna", "$2a$10$0.XQQChT9PUlSLKhXe4kweNqkCqcdizWVh45CmZxJ5gEPx5EWm/J2", "anna@example.com", 3, "Anna Malassu", "+358410000000", "admin");
+INSERT INTO Education (user_id, school, degree, field, graduation) VALUES (1, 'ammattikoulu', 'ammattitutkinto', 'matkailu', '2022-06-01'), (2, 'amk', 'alempi korkeakoulututkinto', 'it', '2023-08-20'), (3, 'amk', 'ylempi korkeakoulututkinto', 'tietoturva', '2024-03-24');
+INSERT INTO JobExperience (user_id, job_title, job_place, job_city, start_date) VALUES (1, 'barista', 'cafe', 'Helsinki', '2022-10-10'), (2, 'hr-manager', 'Kamilla Oy', 'Espoo', '2023-11-11'), (3, 'admin', 'JobMeIn Oy', 'Helsinki', '2024-03-24');
+INSERT INTO Attachments (attachment_name, user_id, link) VALUES ('Resume', 1, 'https://example.com/resume.pdf'), ('Cover Letter', 2, 'https://example.com/cover_letter.docx'), ('Portfolio', 3, 'https://example.com/portfolio.pdf');
+INSERT INTO JobAds (job_address, job_title, salary, user_id, job_description, deadline_date, field) VALUES ('Helsinki', 'Software Engineer', '7000', 2, 'Etsitään senior software engineerin', '2024-04-30', 'IT'), ('Vantaa', 'Tarjoilija', '1200', 2, 'Kesätöitä opiskelijoille', '2024-04-15', 'Ravintola- ja catering'), ('Joensuu', 'Marjamyyjä', '800', 2, 'Kesätöitä Joensuun torille toukokuusta elokuuhun', '2024-05-15', 'Kauppa');
+INSERT INTO Skills (skill_name, type) VALUES ('Java', 'Kova'), ('Asiakaspalvelu', 'Pehmeä'), ('Suomen kieli', 'Kova');
+INSERT INTO JobSkills (skill_id, job_id) VALUES (1, 1), (2, 2), (3, 3);
+INSERT INTO UserSkills (user_id, skill_id) VALUES (1, 1), (1, 2), (1, 3);
+INSERT INTO KeyWords (keyword_name) VALUES ('Full Stack'), ('Ravintola'), ('Marjamyynti');
+INSERT INTO KeywordsJobs (keyword_id, job_id) VALUES (1, 1), (2, 2), (3, 3);
+INSERT INTO Applications (user_id, job_id, status) VALUES (1, 2, 'Pending'), (1, 1, 'Submitted'), (1, 3, 'Pending');
+INSERT INTO ApplicationLinks (application_id, link) VALUES (1, 'https://example.com/application1'), (2, 'https://example.com/application2'), (3, 'https://example.com/application3');
+INSERT INTO Tests (test_type, user_id, test_link) VALUES ('Java-testi',  2, 'https://example.com/java_test'), ('Persoonallisuustesti', NULL, 'https://example.com/personality_test'), ('Empatiatesti', 2, 'https://example.com/empathy_test');
+INSERT INTO JobTests (job_id, test_id) VALUES (1, 1), (2, 2), (3, 3);
+INSERT INTO UserTests (test_id, user_id, percentage, completed_at) VALUES (1, 1, 85, '2024-03-24'), (3, 1, 70, '2024-03-23');
+INSERT INTO Chats (user1_id, user2_id) VALUES (1, 2), (2, 3);
+INSERT INTO Messages (user_id, chat_id, message_text) VALUES (1, 1, 'Hei, olen maailman paras ohjelmistokehittäjä'), (2, 1, 'Ok, kiva');
+INSERT INTO Swipes (swiper_id, swiped_id, swipe_direction) VALUES (1, 2, 'Right'), (2, 1, 'Right');
+INSERT INTO Matches (user1_id, user2_id) VALUES (1, 2);
+INSERT INTO Reports (user_id, reported_item_type, reported_item_id, report_reason) VALUES (2, 'User', 1, 'Kysyi henkilökohtaisia kysymyksiä'), (1, 'Job', 3, 'Marjamyyjä ei voi saada 800 euroa');
+INSERT INTO Animals (animal_name) VALUES ('Leijona'), ('Kissa'), ('Delfiini');
+INSERT INTO Adjectives (adj_name) VALUES ('Ystävällinen'), ('Ahkera'), ('Luova');
+INSERT INTO Notifications (match_id) VALUES (1);
