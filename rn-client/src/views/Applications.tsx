b057fc9fbfc278756418fc7d8c14a598efb3d3a8
Kamilla <KamillaKa@users.noreply.github.com>
2024-04-03 19:28:39 +0300

    first draft

--- rn-client/src/views/Applications.tsx
+++ rn-client/src/views/Applications.tsx
@@ -1,12 +1,26 @@
-import {Text, View} from 'react-native';
-import {useEffect} from 'react';
-import {useUserContext} from '../hooks/ContextHooks';
+import React from 'react';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+import {StyleSheet} from 'react-native';
+import Applied from '../components/Applied';
+import Saved from '../components/Saved';
+
+const Tab = createMaterialTopTabNavigator();
 
 const Applications = () => {
   return (
-    <View>
-      <Text>Applications</Text>
-    </View>
+    <Tab.Navigator
+      initialRouteName="Saved"
+      screenOptions={{
+        tabBarActiveTintColor: '#004aad',
+        tabBarInactiveTintColor: 'grey',
+        tabBarIndicatorStyle: {backgroundColor: '#004aad'},
+        tabBarLabelStyle: {fontWeight: 'bold'},
+        tabBarStyle: {backgroundColor: 'white'},
+      }}
+    >
+      <Tab.Screen name="Saved" component={Saved} />
+      <Tab.Screen name="Applied" component={Applied} />
+    </Tab.Navigator>
   );
 };
 
