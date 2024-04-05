f3e4f1843ceb9c80b45f94184a96cc40bd640be0
Iaroslava Goncharova <111697910+iaroslavagoncharova@users.noreply.github.com>
2024-04-05 16:33:58 +0300

    job ad cards: added company info and job details

--- rn-client/src/components/JobAd.tsx
+++ rn-client/src/components/JobAd.tsx
@@ -1,17 +1,30 @@
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
-import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
+import React, {useEffect, useState} from 'react';
 import {Card} from '@rneui/base';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import {
   faArrowAltCircleLeft,
   faArrowAltCircleRight,
 } from '@fortawesome/free-solid-svg-icons';
-import {JobWithSkillsAndKeywords} from '../types/DBTypes';
+import {JobWithSkillsAndKeywords, User} from '../types/DBTypes';
+import {useUser} from '../hooks/apiHooks';
+import useUpdateContext from '../hooks/updateHooks';
 
 export default function JobAd({job}: {job: JobWithSkillsAndKeywords}) {
+  const [currentScreen, setCurrentScreen] = useState<number>(0);
+  const [user, setUser] = useState<User | null>(null);
+  const {update} = useUpdateContext();
+  const {getUserById} = useUser();
   const styles = StyleSheet.create({
     card: {
-      width: '90%',
+      width: 350,
+      height: 500,
       margin: 50,
       padding: 20,
       borderRadius: 25,
@@ -34,6 +47,19 @@ export default function JobAd({job}: {job: JobWithSkillsAndKeywords}) {
       margin: 5,
     },
     header3: {
+      color: '#5d71c9',
+      fontSize: 15,
+      fontWeight: 'bold',
+      textAlign: 'center',
+      margin: 5,
+    },
+    text: {
+      color: '#5d71c9',
+      fontSize: 15,
+      textAlign: 'center',
+      margin: 5,
+    },
+    boldText: {
       color: '#5d71c9',
       fontSize: 15,
       fontWeight: 'bold',
@@ -74,7 +100,7 @@ export default function JobAd({job}: {job: JobWithSkillsAndKeywords}) {
       borderRadius: 25,
       width: 50,
       height: 50,
-      top: 40,
+      top: -10,
       left: 70,
     },
     buttonLeft: {
@@ -82,40 +108,169 @@ export default function JobAd({job}: {job: JobWithSkillsAndKeywords}) {
       borderRadius: 25,
       width: 50,
       height: 50,
-      bottom: 5,
+      top: 35,
       right: 60,
     },
   });
+
+  const addEmployerInfo = async () => {
+    const user = await getUserById(job.user_id);
+    setUser(user);
+    console.log(user);
+  };
+
+  useEffect(() => {
+    addEmployerInfo();
+  }, [update]);
+
+  const renderContent = () => {
+    switch (currentScreen) {
+      case 0:
+        return (
+          <>
+            <Text style={styles.header1}>{job.job_title}</Text>
+            <Text style={styles.header2}>Ala: {job.field}</Text>
+            <Text style={styles.header2}>Yritys: {job.username}</Text>
+            <Text style={styles.header3}>Sijainti: {job.job_address}</Text>
+            <Text style={styles.header3}>Palkka: {job.salary}€/kk</Text>
+            <Text style={styles.percent}>56%</Text>
+            <Text style={styles.skills}>{job.skills}</Text>
+            <Text style={styles.keywords}>{job.keywords}</Text>
+          </>
+        );
+      case 1:
+        return (
+          <>
+            <Text style={styles.header2}>{job.job_title}</Text>
+            <Text style={styles.header2}>Yritys: {job.username}</Text>
+            <Text style={styles.header3}>Ala: {job.field}</Text>
+            <Text style={styles.header3}>Sijainti: {job.job_address}</Text>
+            <Text style={styles.header3}>Palkka: {job.salary}€/kk</Text>
+            <Text
+              style={{
+                color: '#ffffff',
+                backgroundColor: '#004AAD',
+                borderRadius: 25,
+                padding: 10,
+                textAlign: 'center',
+              }}
+            >
+              Viimeinen hakupäivä:{' '}
+              {job.deadline_date
+                ? job.deadline_date
+                    .toString()
+                    .substring(0, 10)
+                    .split('-')
+                    .reverse()
+                    .join('.')
+                : 'Ei määritelty'}
+            </Text>
+            <Text style={styles.header3}>Työpaikan kuvaus: </Text>
+            <Text style={styles.text}>{job.job_description}</Text>
+            <Text style={styles.header3}>Taidot: </Text>
+            {job.skills.split(',').map((skill, index) => (
+              <Text key={index} style={styles.text}>
+                {skill}
+              </Text>
+            ))}
+            {job.keywords.split(',').map((keyword, index) => (
+              <Text key={index} style={styles.boldText}>
+                #{keyword}
+              </Text>
+            ))}
+          </>
+        );
+      case 2:
+        return (
+          <>
+            <Text style={styles.header2}>Työnantajan tiedot</Text>
+            <Text style={styles.header2}>Yritys: {user?.username}</Text>
+            <Text style={styles.header3}>
+              Tiedot: {user?.about_me ? user.about_me : 'Ei määritelty'}
+            </Text>
+            <Text style={styles.header3}>
+              Osoite: {user?.address ? user.address : 'Ei määritelty'}
+            </Text>
+            <Card>
+              <Text style={styles.header3}>Yhteystiedot</Text>
+              <Text style={styles.header3}>Yhteyshenkilö:</Text>
+              <Text style={styles.text}>
+                {user?.fullname ? user.fullname : 'Ei määritelty'}
+              </Text>
+              <Text style={styles.header3}>Puhelin:</Text>
+              <Text style={styles.text}>
+                {user?.phone ? user.phone : 'Ei määritelty'}
+              </Text>
+              <Text style={styles.header3}>Sähköposti:</Text>
+              <Text style={styles.text}>
+                {user?.email ? user.email : 'Ei määritelty'}
+              </Text>
+            </Card>
+          </>
+        );
+      default:
+        return null;
+    }
+  };
+
+  const navigateToNextScreen = () => {
+    setCurrentScreen(currentScreen + 1);
+  };
+
+  const navigateToPreviousScreen = () => {
+    setCurrentScreen(currentScreen - 1);
+  };
+
   return (
-    <View
-      style={{
-        alignItems: 'center',
-      }}
-    >
-      <Text></Text>
-      <Card containerStyle={styles.card}>
-        <Text style={styles.header1}>{job.job_title}</Text>
-        <Text style={styles.header2}>Yritys: {job.username}</Text>
-        <Text style={styles.header3}>Sijainti: {job.job_address}</Text>
-        <Text style={styles.header3}>Palkka: {job.salary}€/kk</Text>
-        <Text style={styles.percent}>56%</Text>
-        <Text style={styles.skills}>{job.skills}</Text>
-        <Text style={styles.keywords}>{job.keywords}</Text>
-      </Card>
-      <TouchableOpacity>
-        <FontAwesomeIcon
-          icon={faArrowAltCircleRight}
-          size={45}
-          style={styles.buttonRight}
-        />
-      </TouchableOpacity>
-      <TouchableOpacity>
+    <View style={{alignItems: 'center', backgroundColor: '#5d71c9'}}>
+      <Card containerStyle={styles.card}>{renderContent()}</Card>
+      <TouchableOpacity onPress={navigateToPreviousScreen}>
         <FontAwesomeIcon
           icon={faArrowAltCircleLeft}
           size={45}
           style={styles.buttonLeft}
         />
       </TouchableOpacity>
+      <TouchableOpacity onPress={navigateToNextScreen}>
+        <FontAwesomeIcon
+          icon={faArrowAltCircleRight}
+          size={45}
+          style={styles.buttonRight}
+        />
+      </TouchableOpacity>
     </View>
   );
 }
+// return (
+//   <View
+//     style={{
+//       alignItems: 'center',
+//     }}
+//   >
+//     <Text></Text>
+//     <Card containerStyle={styles.card}>
+//       <Text style={styles.header1}>{job.job_title}</Text>
+//       <Text style={styles.header2}>Yritys: {job.username}</Text>
+//       <Text style={styles.header3}>Sijainti: {job.job_address}</Text>
+//       <Text style={styles.header3}>Palkka: {job.salary}€/kk</Text>
+//       <Text style={styles.percent}>56%</Text>
+//       <Text style={styles.skills}>{job.skills}</Text>
+//       <Text style={styles.keywords}>{job.keywords}</Text>
+//     </Card>
+//     <TouchableOpacity>
+//       <FontAwesomeIcon
+//         icon={faArrowAltCircleRight}
+//         size={45}
+//         style={styles.buttonRight}
+//       />
+//     </TouchableOpacity>
+//     <TouchableOpacity>
+//       <FontAwesomeIcon
+//         icon={faArrowAltCircleLeft}
+//         size={45}
+//         style={styles.buttonLeft}
+//       />
+//     </TouchableOpacity>
+//   </View>
+// );
+// }
